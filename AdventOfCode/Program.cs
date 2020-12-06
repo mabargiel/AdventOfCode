4cf1a555559ff29b2f7f8cdadd9b026e944ef2c2
Mateusz Bargiel <m.bargiel@avanade.com>
2020-12-06 10:18:10 +0100

    Implement Day4 2020

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using AdventOfCode.Days._2020._3;
+using AdventOfCode.Days._2020._4;
 
 namespace AdventOfCode
 {
@@ -9,11 +9,11 @@ namespace AdventOfCode
     {
         private static async Task Main(string[] args)
         {
-            var input = await File.ReadAllTextAsync("2020/3/input.txt");
+            var input = await File.ReadAllTextAsync("2020/4/input.txt");
 
-            var d3 = new Day3(input);
+            var d4 = new Day4(input);
 
-            var result = d3.Part2();
+            var result = d4.Part2();
 
             Console.WriteLine(result);
         }
