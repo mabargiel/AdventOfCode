067d8f484305e21f2a0262f103dc4703189ef640
Mateusz Bargiel <mbargiel@btopservice.net>
2021-12-01 13:59:02 +0100

    Implement a new way of displaying results using command line arguments

---
+++ AdventOfCode.Days/2021/Day1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Days._2021
+{
+    public class Day1 : AdventDay<int[], int, int>
+    {
+        public override int[] ParseRawInput(string rawInput)
+        {
+            return rawInput.Split(Environment.NewLine).Select(int.Parse).ToArray();
+        }
+
+        public override int Part1(int[] input)
+        {
+            var result = 0;
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i + 1] > input[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        public override int Part2(int[] input)
+        {
+            var result = 0;
+            for (var i = 3; i < input.Length; i++)
+            {
+                if (input[i - 3] < input[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- AdventOfCode.Days/IAdventDay.cs
+++ AdventOfCode.Days/IAdventDay.cs
@@ -1,5 +1,28 @@
 ﻿namespace AdventOfCode.Days
 {
+    public abstract class AdventDay<TIn, TOut1, TOut2> : IAdventDay
+    {
+        public string Part1(string rawInput)
+        {
+            return Part1(ParseRawInput(rawInput)).ToString();
+        }
+
+        public string Part2(string rawInput)
+        {
+            return Part2(ParseRawInput(rawInput)).ToString();
+        }
+
+        public abstract TIn ParseRawInput(string rawInput);
+        public abstract TOut1 Part1(TIn input);
+        public abstract TOut2 Part2(TIn input);
+    }
+
+    public interface IAdventDay
+    {
+        string Part1(string rawInput);
+        string Part2(string rawInput);
+    }
+    
     public interface IAdventDay<out TOut1, out TOut2>
     {
         TOut1 Part1();
