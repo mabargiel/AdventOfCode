787628d094d621286a0e60fdaa3d58d5f0d341f6
Mateusz Bargiel <mbargiel@btopservice.net>
2021-12-11 15:10:07 +0100

    Solve Day 11 Year 2021

---
+++ AdventOfCode.Days/2021/Day11.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Days.Common;
+
+namespace AdventOfCode.Days._2021
+{
+    public class Day11 : AdventDay<int[,], int, int>
+    {
+        public override int[,] ParseRawInput(string rawInput)
+        {
+            var rows = rawInput.Trim().Split(Environment.NewLine);
+            var result = new int[10, 10];
+            
+            for (var y = 0; y < 10; y++)
+            {
+                for (var x = 0; x < 10; x++)
+                {
+                    result[y, x] = (int) char.GetNumericValue(rows[y][x]);
+                }
+            }
+
+            return result;
+        }
+
+        public override int Part1(int[,] input)
+        {
+            var result = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                input = FlashOctopuses(IncreaseEnergyLevel(input));
+                result += input.Count(o => o == 0);
+            }
+
+            return result;
+        }
+
+        public override int Part2(int[,] input)
+        {
+            var result = 0;
+            while(input.Any(o => o != 0))
+            {
+                result++;
+                input = FlashOctopuses(IncreaseEnergyLevel(input));
+            }
+
+            return result;
+        }
+
+        private static int[,] IncreaseEnergyLevel(int[,] octopuses)
+        {
+            var result = new int[10,10];
+            for (var y = 0; y < 10; y++)
+            {
+                for (var x = 0; x < 10; x++)
+                {
+                    result[y, x] = octopuses[y, x] + 1;
+                }
+            }
+
+            return result;
+        }
+
+        private static int[,] FlashOctopuses(int[,] octopuses)
+        {
+            var result = (int[,]) octopuses.Clone();
+            var theyWillFLash = result.Where(o => o > 9).ToArray();
+            do
+            {
+                foreach (var (y, x) in theyWillFLash)
+                {
+                    result[y, x] = 0;
+
+                    foreach(var yy in Enumerable.Range(y - 1, 3))
+                    {
+                        if (yy is < 0 or >= 10)
+                        {
+                            continue;
+                        }
+                        
+                        foreach (var xx in Enumerable.Range(x - 1, 3))
+                        {
+                            if (xx is < 0 or >= 10 || result[yy, xx] == 0)
+                            {
+                                continue;
+                            }
+
+                            result[yy, xx]++;
+                        }
+                    }
+                }
+                
+                theyWillFLash = result.Where(o => o > 9).ToArray();
+            } while (theyWillFLash.Any());
+
+            return result;
+        }
+    }
+}
--- AdventOfCode.Days/Common/ArrExt.cs
+++ AdventOfCode.Days/Common/ArrExt.cs
@@ -7,6 +7,53 @@ namespace AdventOfCode.Days.Common
 {
     public static class ArrExt
     {
+        public static int Count<T>(this T[,] array, Func<T, bool> predicate)
+        {
+            var result = 0;
+            for (var y = 0; y < array.GetLength(0); y++)
+            {
+                for (var x = 0; x < array.GetLength(1); x++)
+                {
+                    if (predicate.Invoke(array[y, x]))
+                    {
+                        result++;
+                    }
+                }
+            }
+            
+            return result;
+        }
+        
+        public static bool Any<T>(this T[,] array, Func<T, bool> predicate)
+        {
+            for (var y = 0; y < array.GetLength(0); y++)
+            {
+                for (var x = 0; x < array.GetLength(1); x++)
+                {
+                    if (predicate.Invoke(array[y, x]))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+        public static IEnumerable<(int Y, int X)> Where<T>(this T[,] array, Func<T, bool> predicate)
+        {
+            for (var y = 0; y < array.GetLength(0); y++)
+            {
+                for (var x = 0; x < array.GetLength(1); x++)
+                {
+                    if (predicate.Invoke(array[y, x]))
+                    {
+                        yield return (y, x);
+                    }
+                }
+            }
+        }
+        
         public static IEnumerable<T> GetRow<T>(this T[,] array, int row)
         {
             if (!typeof(T).IsPrimitive)
