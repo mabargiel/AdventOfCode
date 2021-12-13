18a3982236b226ff2fa5ec25fd3b5a93fd5c8390
Mateusz Bargiel <mbargiel@btopservice.net>
2021-12-13 15:02:14 +0100

    Solve Day 13 Year 2021

--- AdventOfCode.Days/2021/Day11.cs
+++ AdventOfCode.Days/2021/Day11.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Days.Common;
 
@@ -11,12 +10,12 @@ namespace AdventOfCode.Days._2021
         {
             var rows = rawInput.Trim().Split(Environment.NewLine);
             var result = new int[10, 10];
-            
+
             for (var y = 0; y < 10; y++)
             {
                 for (var x = 0; x < 10; x++)
                 {
-                    result[y, x] = (int) char.GetNumericValue(rows[y][x]);
+                    result[y, x] = (int)char.GetNumericValue(rows[y][x]);
                 }
             }
 
@@ -28,7 +27,7 @@ namespace AdventOfCode.Days._2021
             var result = 0;
             for (var i = 0; i < 100; i++)
             {
-                input = FlashOctopuses(IncreaseEnergyLevel(input));
+                input = Flash(IncreaseEnergyLevel(input));
                 result += input.Count(o => o == 0);
             }
 
@@ -38,10 +37,10 @@ namespace AdventOfCode.Days._2021
         public override int Part2(int[,] input)
         {
             var result = 0;
-            while(input.Any(o => o != 0))
+            while (input.Any(o => o != 0))
             {
                 result++;
-                input = FlashOctopuses(IncreaseEnergyLevel(input));
+                input = Flash(IncreaseEnergyLevel(input));
             }
 
             return result;
@@ -49,7 +48,7 @@ namespace AdventOfCode.Days._2021
 
         private static int[,] IncreaseEnergyLevel(int[,] octopuses)
         {
-            var result = new int[10,10];
+            var result = new int[10, 10];
             for (var y = 0; y < 10; y++)
             {
                 for (var x = 0; x < 10; x++)
@@ -61,23 +60,24 @@ namespace AdventOfCode.Days._2021
             return result;
         }
 
-        private static int[,] FlashOctopuses(int[,] octopuses)
+        private static int[,] Flash(int[,] octopuses)
         {
-            var result = (int[,]) octopuses.Clone();
-            var theyWillFLash = result.Where(o => o > 9).ToArray();
-            do
+            var result = (int[,])octopuses.Clone();
+            (int Y, int X)[] theyWillFLash;
+
+            while ((theyWillFLash = result.Where(o => o > 9).ToArray()).Any())
             {
                 foreach (var (y, x) in theyWillFLash)
                 {
                     result[y, x] = 0;
 
-                    foreach(var yy in Enumerable.Range(y - 1, 3))
+                    foreach (var yy in Enumerable.Range(y - 1, 3))
                     {
                         if (yy is < 0 or >= 10)
                         {
                             continue;
                         }
-                        
+
                         foreach (var xx in Enumerable.Range(x - 1, 3))
                         {
                             if (xx is < 0 or >= 10 || result[yy, xx] == 0)
@@ -89,9 +89,7 @@ namespace AdventOfCode.Days._2021
                         }
                     }
                 }
-                
-                theyWillFLash = result.Where(o => o > 9).ToArray();
-            } while (theyWillFLash.Any());
+            }
 
             return result;
         }
---
+++ AdventOfCode.Tests/2021/Day12.cs
@@ -0,0 +1,121 @@
+using System.Collections.Generic;
+using AdventOfCode.Days._2021;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2021
+{
+    public class Day12 : AdventDayTest<Days._2021.Day12>
+    {
+        [Test]
+        public override void ParseRawInputTest()
+        {
+            const string rawInput = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+
+            var input = _day.ParseRawInput(rawInput);
+            
+            input.AdjacentList["start"].ShouldBeEquivalentTo(new HashSet<string> {"A", "b"});
+            input.AdjacentList["A"].ShouldBeEquivalentTo(new HashSet<string> { "start", "c", "b", "end" });
+            input.AdjacentList["b"].ShouldBeEquivalentTo(new HashSet<string> { "start", "A", "d", "end" });
+            input.AdjacentList["c"].ShouldBeEquivalentTo(new HashSet<string> { "A" });
+            input.AdjacentList["d"].ShouldBeEquivalentTo(new HashSet<string> { "b" });
+            input.AdjacentList["end"].ShouldBeEquivalentTo(new HashSet<string> { "A", "b" });
+        }
+
+        [Test]
+        [TestCase(@"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end", 10)]
+        [TestCase(@"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc", 19)]
+        [TestCase(@"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW", 226)]
+        public void Part1_FindUniquePathsCount(string rawInput, int expectedResult)
+        {
+            var input = new Graph(rawInput);
+
+            var result = _day.Part1(input);
+            
+            result.ShouldBe(expectedResult);
+        }
+        
+        [Test]
+        [TestCase(@"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end", 36)]
+        [TestCase(@"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc", 103)]
+        [TestCase(@"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW", 3509)]
+        public void Part2_FindUniquePathsWithVisitingSmallCaveTwiceCount(string rawInput, int expectedResult)
+        {
+            var input = new Graph(rawInput);
+
+            var result = _day.Part2(input);
+            
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
