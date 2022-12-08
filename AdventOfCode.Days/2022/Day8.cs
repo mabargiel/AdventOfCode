c4492d62ac8f7bdb881df67468e9bb1ef0f82316
mbargiel <31691087+mabargiel@users.noreply.github.com>
2022-12-08 15:15:41 +0100

    Solve Day 8 2022 (#35)

---
+++ AdventOfCode.Days/2022/Day8.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Days._2022;
+
+public class Day8 : AdventDay<int[,], int, int>
+{
+    public override int[,] ParseRawInput(string rawInput)
+    {
+        var lines = rawInput.Trim().Split(Environment.NewLine);
+        var width = lines[0].Length;
+        var height = lines.Length;
+
+        var result = new int [height, width];
+
+        for (var i = 0; i < height; i++)
+        {
+            for (var j = 0; j < width; j++)
+            {
+                result[i, j] = (int)char.GetNumericValue(lines[i][j]);
+            }
+        }
+
+        return result;
+    }
+
+    public override int Part1(int[,] input)
+    {
+        var width = input.GetLength(1);
+        var height = input.GetLength(0);
+        var result = 2 * width + 2 * height - 4;
+
+        for (var i = 1; i < height - 1; i++)
+        {
+            for (var j = 1; j < width - 1; j++)
+            {
+                var column = GetColumn(input, j, height);
+                var row = GetRow(input, i, width);
+                var treeHeight = input[i, j];
+
+                if (column[..i].All(v => v < treeHeight) ||
+                    column[(i + 1)..].All(v => v < treeHeight) ||
+                    row[..j].All(v => v < treeHeight) ||
+                    row[(j + 1)..].All(v => v < treeHeight))
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public override int Part2(int[,] input)
+    {
+        var width = input.GetLength(1);
+        var height = input.GetLength(0);
+        var result = 0;
+
+        for (var i = 1; i < height - 1; i++)
+        {
+            for (var j = 1; j < width - 1; j++)
+            {
+                var column = GetColumn(input, j, height);
+                var row = GetRow(input, i, width);
+                var treeHeight = input[i, j];
+
+                var treesPerDirection = new[]
+                {
+                    column[(i + 1)..], column[..i].Reverse().ToArray(), row[(j + 1)..], row[..j].Reverse().ToArray()
+                };
+
+                var viewDistances = treesPerDirection.Select(x =>
+                {
+                    var firstHigherTree = x.FirstOrDefault(y => y >= treeHeight);
+                    return firstHigherTree == default ? x.Length : Array.IndexOf(x, firstHigherTree) + 1;
+                }).ToArray();
+                var scenicScore = viewDistances.Aggregate((curr, prev) => curr * prev);
+                result = result < scenicScore ? scenicScore : result;
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] GetColumn(int[,] grid, int columnNumber, int height)
+    {
+        return Enumerable.Range(0, height)
+            .Select(x => grid[x, columnNumber])
+            .ToArray();
+    }
+
+    private static int[] GetRow(int[,] grid, int rowNumber, int width)
+    {
+        return Enumerable.Range(0, width)
+            .Select(x => grid[rowNumber, x])
+            .ToArray();
+    }
+}
---
+++ AdventOfCode.Tests/2022/Day8.cs
@@ -0,0 +1,86 @@
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2022;
+
+public class Day8
+{
+    private Days._2022.Day8 _day8;
+
+    [SetUp]
+    public void Initialize()
+    {
+        _day8 = new Days._2022.Day8();
+    }
+
+    [Test]
+    public void ParseRawInput_WithExampleInput_SplitsItAGridOfHeights()
+    {
+        var rawInput = @"30373
+25512
+65332
+33549
+35390";
+
+        var input = _day8.ParseRawInput(rawInput);
+        input.ShouldBe(new[,]
+        {
+            { 3, 0, 3, 7, 3 },
+            { 2, 5, 5, 1, 2 },
+            { 6, 5, 3, 3, 2 },
+            { 3, 3, 5, 4, 9 },
+            { 3, 5, 3, 9, 0 }
+        });
+    }
+
+    [Test]
+    public void Part1_WithExampleInput_CountVisibleTrees()
+    {
+        var input = new[,]
+        {
+            { 3, 0, 3, 7, 3 },
+            { 2, 5, 5, 1, 2 },
+            { 6, 5, 3, 3, 2 },
+            { 3, 3, 5, 4, 9 },
+            { 3, 5, 3, 9, 0 }
+        };
+
+        var result = _day8.Part1(input);
+
+        result.ShouldBe(21);
+    }
+
+    [Test]
+    public void Part1_WithExampleSquareInput_FindBestSpotToBuildAHouse()
+    {
+        var input = new[,]
+        {
+            { 3, 0, 3, 7, 3 },
+            { 2, 5, 5, 1, 2 },
+            { 6, 5, 3, 3, 2 },
+            { 3, 3, 5, 4, 9 },
+            { 3, 5, 3, 9, 0 }
+        };
+
+        var result = _day8.Part2(input);
+
+        result.ShouldBe(8);
+    }
+
+    [Test]
+    public void Part1_WithExampleInput_FindBestSpotToBuildAHouse()
+    {
+        var input = new[,]
+        {
+            { 9, 8, 7, 6, 5, 4 },
+            { 8, 5, 5, 1, 2, 2 },
+            { 7, 5, 3, 3, 2, 8 },
+            { 6, 3, 5, 4, 9, 1 },
+            { 5, 5, 3, 9, 0, 7 }
+        };
+
+        var result = _day8.Part2(input);
+
+        result.ShouldBe(12);
+    }
+}
