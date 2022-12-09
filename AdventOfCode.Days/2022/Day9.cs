83a3874ec55b038555949712d0f3fdd1f1727b6f
mbargiel <31691087+mabargiel@users.noreply.github.com>
2022-12-09 16:54:08 +0100

    Solve Day 9 2022 (#36)

---
+++ AdventOfCode.Days/2022/Day9.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Days.Common;
+
+namespace AdventOfCode.Days._2022;
+
+public class Day9 : AdventDay<Motion[], int, int>
+{
+    public override Motion[] ParseRawInput(string rawInput)
+    {
+        return rawInput.Trim().Split(Environment.NewLine)
+            .Select(x => new Motion(x.First(), int.Parse(x[(x.IndexOf(' ') + 1)..]))).ToArray();
+    }
+
+    public override int Part1(Motion[] input)
+    {
+        return VisitedCount(input, 1);
+    }
+
+    public override int Part2(Motion[] input)
+    {
+        return VisitedCount(input, 9);
+    }
+
+    private static int VisitedCount(IEnumerable<Motion> input, int tailLength)
+    {
+        var head = new Point(0, 0);
+        var tail = new List<Point>(Enumerable.Range(0, tailLength).Select(x => new Point(0, 0)));
+        var visited = new HashSet<Point> { new(0, 0) };
+
+        foreach (var motion in input)
+        {
+            Func<Point, Point> move = motion.Direction switch
+            {
+                'U' => p => p with { Y = p.Y + 1 },
+                'D' => p => p with { Y = p.Y - 1 },
+                'L' => p => p with { X = p.X - 1 },
+                'R' => p => p with { X = p.X + 1 },
+                _ => p => p
+            };
+
+            for (var i = 0; i < motion.Steps; i++)
+            {
+                head = move(head);
+                var curr = head;
+
+                for (var j = 0; j < tailLength; j++)
+                {
+                    var tailPart = tail[j];
+                    var xDiff = curr.X - tailPart.X;
+                    var yDiff = curr.Y - tailPart.Y;
+                    var xDistance = Math.Abs(xDiff);
+                    var yDistance = Math.Abs(yDiff);
+
+                    if (xDistance > 1 || yDistance > 1)
+                    {
+                        xDiff += xDistance > 1 ? curr.X < tailPart.X ? 1 : -1 : 0;
+                        yDiff += yDistance > 1 ? curr.Y < tailPart.Y ? 1 : -1 : 0;
+                        tail[j] = new Point(tailPart.X + xDiff, tailPart.Y + yDiff);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    curr = tail[j];
+                }
+
+                visited.Add(tail[tailLength - 1]);
+            }
+        }
+
+        return visited.Count;
+    }
+}
+
+public record Motion(char Direction, int Steps);
+
---
+++ AdventOfCode.Tests/2022/Day9.cs
@@ -0,0 +1,82 @@
+using AdventOfCode.Days._2022;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2022;
+
+public class Day9
+{
+    private Days._2022.Day9 _day9;
+
+    [SetUp]
+    public void Initialize()
+    {
+        _day9 = new Days._2022.Day9();
+    }
+
+    [Test]
+    public void ParseRawInput_WithExampleInput_SplitsItAGridOfHeights()
+    {
+        var rawInput = @"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 22";
+
+        var input = _day9.ParseRawInput(rawInput);
+        input.ShouldBe(new Motion[]
+        {
+            new('R', 4),
+            new('U', 4),
+            new('L', 3),
+            new('D', 1),
+            new('R', 4),
+            new('D', 1),
+            new('L', 5),
+            new('R', 22)
+        });
+    }
+
+    [Test]
+    public void Part1_ExampleInput_FollowStepsAndReturnAllPosVisitedByShortTail()
+    {
+        var input = new Motion[]
+        {
+            new('R', 4),
+            new('U', 4),
+            new('L', 3),
+            new('D', 1),
+            new('R', 4),
+            new('D', 1),
+            new('L', 5),
+            new('R', 2)
+        };
+
+        var result = _day9.Part1(input);
+
+        result.ShouldBe(13);
+    }
+
+    [Test]
+    public void Part2_ExampleInput_FollowStepsAndReturnAllPosVisitedByLongTail()
+    {
+        var input = new Motion[]
+        {
+            new('R', 5),
+            new('U', 8),
+            new('L', 8),
+            new('D', 3),
+            new('R', 17),
+            new('D', 10),
+            new('L', 25),
+            new('U', 20)
+        };
+
+        var result = _day9.Part2(input);
+
+        result.ShouldBe(36);
+    }
+}
